d3e8b5fdffc2b9bed526487f9cb83fc541b55ef5
Sibonelo Lukhele <sibonelolukhele600@gmail.com>
2025-04-15 11:54:35 +0200

    another commit

--- CurrencyConvertor.py
+++ CurrencyConvertor.py
@@ -1,9 +1,29 @@
 usd = 1.00
 zar = 18.25
+ask = True
+
+def inputamount():
+
+    while ask:
+        answer = input("Enter how much you'd like to convert TO ZAR (in USD) or enter done to exit: ")
+        
+        if answer.isdigit():
+            calculation(int(answer))
+
+        elif answer.lower() == "done":
+            print("See you next time!")
+            loop=False
+            break
+
+        else:
+            print("\nPlease enter a numeric value or enter done to exit!\n")
+
+def calculation(answer):    
+    con = float(answer)* zar
+    print(f"\nYou have R{round(con, 2)}\n")
+
+
 
 print("Welcome to the Currency Converter\n")
+inputamount()
 
-answer = input("Enter how much you'd like to convert (in USD): ")
-con = float(answer)* zar
-
-print("\nYou have", round(con, 2), "ZAR")
